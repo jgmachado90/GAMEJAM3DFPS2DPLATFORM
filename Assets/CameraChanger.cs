faaba54e5f70ade0c30c8fa225229a722515511f
joao <jgmachado@inf.ufsm.br>
2020-11-14 00:31:15 -0300

    2d platform changer working

--- Assets/CameraChanger.cs
+++ Assets/CameraChanger.cs
@@ -6,19 +6,21 @@ public class CameraChanger : MonoBehaviour
 {
     public move lastPlayer;
     public move nextPlayer;
+
+    public Camera lastCamera;
     public Camera nextCamera;
-    public Camera firstCamera;
+
     public Transform lastSpawn;
     public Transform nextSpawn;
 
     public void CameraChange(Transform lastPlayerTransform)
     {
-        firstCamera.gameObject.SetActive(false);
+        lastCamera.gameObject.SetActive(false);
         nextCamera.gameObject.SetActive(true);
         
-        Camera aux = firstCamera;
+        Camera aux = lastCamera;
 
-        firstCamera = nextCamera;
+        lastCamera = nextCamera;
         nextCamera = aux;
 
         lastPlayer = lastPlayerTransform.GetComponent<move>();
@@ -26,6 +28,9 @@ public class CameraChanger : MonoBehaviour
         nextPlayer.gameObject.SetActive(true);
         nextPlayer.transform.position = new Vector3(nextSpawn.position.x, lastPlayerTransform.position.y, nextSpawn.position.z);
 
+        Transform auxSpawn = nextSpawn;
+        nextSpawn = lastSpawn;
+        lastSpawn = auxSpawn;
         
         
         move auxplayer = nextPlayer;
