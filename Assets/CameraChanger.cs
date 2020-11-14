3d46c03d4ede18d729ee06c7d113a6fcd3304a90
joao <jgmachado@inf.ufsm.br>
2020-11-14 00:22:58 -0300

    [FEAT] first commit
    this is the first commit

---
+++ Assets/CameraChanger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChanger : MonoBehaviour
+{
+    public move lastPlayer;
+    public move nextPlayer;
+    public Camera nextCamera;
+    public Camera firstCamera;
+    public Transform lastSpawn;
+    public Transform nextSpawn;
+
+    public void CameraChange(Transform lastPlayerTransform)
+    {
+        firstCamera.gameObject.SetActive(false);
+        nextCamera.gameObject.SetActive(true);
+        
+        Camera aux = firstCamera;
+
+        firstCamera = nextCamera;
+        nextCamera = aux;
+
+        lastPlayer = lastPlayerTransform.GetComponent<move>();
+
+        nextPlayer.gameObject.SetActive(true);
+        nextPlayer.transform.position = new Vector3(nextSpawn.position.x, lastPlayerTransform.position.y, nextSpawn.position.z);
+
+        
+        
+        move auxplayer = nextPlayer;
+        nextPlayer = lastPlayer;
+        lastPlayer = auxplayer;
+
+
+
+        
+
+
+    }
+
+}
---
+++ Assets/move.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpSpeed;
+    public Rigidbody rb;
+    public bool canMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            Debug.Log("vel" + transform.right * moveSpeed);
+            rb.AddForce(-transform.right * moveSpeed, ForceMode.Acceleration);
+            //transform.position = new Vector3(transform.position.x + moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKey(KeyCode.L))
+        {
+            rb.AddForce(transform.right * moveSpeed, ForceMode.Acceleration);
+            //transform.position = new Vector3(transform.position.x - moveSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 2f, 0), ForceMode.Impulse);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        this.gameObject.SetActive(false);
+        other.GetComponent<CameraChanger>().CameraChange(transform);
+        Debug.Log("camera changer");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "CameraChanger")
+        {
+            /*if (this.GetComponent<Rigidbody>().velocity.x > 0)
+            {
+                this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                this.GetComponent<Rigidbody>().AddForce(transform.right * 4, ForceMode.Impulse);
+            }
+            if (this.GetComponent<Rigidbody>().velocity.x < 0)
+            {
+                this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                this.GetComponent<Rigidbody>().AddForce(-transform.right * 4, ForceMode.Impulse);
+            }
+          */
+            this.gameObject.SetActive(false);
+            collision.transform.GetComponent<CameraChanger>().CameraChange(transform);
+            Debug.Log("camera changer");
+        }
+    }
+}
