499f214db57fb429f1fa1a842d5fd814e71158c8
Pheryus <plima@inf.ufsm.br>
2020-11-14 13:41:35 -0300

    [FEAT] Buttons, moving platforms e moving platforms activated by buttons

---
+++ Assets/3DSystem/Scripts/Button3D.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Button3D : ActionObject {
+ 
+    Animator animator;
+
+    bool pressed = false, pressedThisFrame = false;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.CompareTag("PlayerFoot") || other.gameObject.CompareTag("Box")){
+            pressedThisFrame = true;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (pressedThisFrame != pressed){
+            if (pressedThisFrame){
+                animator.SetTrigger("Pressed");  
+                onActive?.Invoke();  
+            }
+
+            else {
+                onDisactive?.Invoke();
+                animator.SetTrigger("Unpressed");
+            }
+        }
+        
+        pressed = pressedThisFrame;
+        
+        pressedThisFrame = false;
+    }
+}
